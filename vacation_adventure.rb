4c02058cb9e777f8d6eee1c9daa4e10238513766
rowlandjl <rowlandjl@vcu.edu>
2015-12-18 20:22:58 -0500

    first commit

---
+++ vacation_adventure.rb
@@ -0,0 +1,9 @@
+adventure = rand(1..3)
+
+if adventure == 1
+  puts "You're going sailing!"
+elsif adventure == 2
+  puts "You're going to an animal reservation!"
+else
+  puts "You're going jetskiing!"
+end
